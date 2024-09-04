f03970d7f8536726c392386c3ecdae25469e08d3
Nick T <nicholas.tayag@ufl.edu>
2024-09-04 16:02:22 -0400

    git squash into one commit!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
+        </p>
+        <p>
+          My name is Nicholas Tayag and this is my project
         </p>
         <a
           className="App-link"
